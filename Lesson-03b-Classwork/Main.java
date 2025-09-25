e1be97aa90ebc75ae3b1ba4aecbbf2a028feb60c
E-10z <ethanl222@nycstudents.net>
2025-09-25 14:13:56 +0000

    update

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Ethan Lin");
+    System.out.println("123 Sesame Street");
+    System.out.println("Ethan Lin \n 123 Sesame Street");
+   
+    
+  }
+
+  
+}
