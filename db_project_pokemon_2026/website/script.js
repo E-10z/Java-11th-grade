66f773faa443079b62e6cf948fbd7621d7c4f459
E-10z <ethanl222@nycstudents.net>
2026-06-12 03:27:45 +0000

    dsadwasd

--- db_project_pokemon_2026/website/script.js
+++ db_project_pokemon_2026/website/script.js
@@ -1,42 +1,41 @@
-async function init_customers() {
-  let link = "https://symmetrical-space-fortnight-r464p9x54g56fp7qq-8000.app.github.dev/Pokemon";
+async function init_pokemon() {
+  let link = "https://symmetrical-space-fortnight-r464p9x54g56fp7qq-8000.app.github.dev/";
   let response = await fetch(link);
   let pokemon = await response.json();
   generateCards(pokemon);
 }
 
 function generateCards(pokemon) {
-  let output = "";
+  let output = document.getElementById("centerpanel");
+  let build = "";
 
-  for (let p of pokemon) {
-    let typeClass = p.type.toLowerCase();
-    typeClass = typeClass.replace(" ", "-");
-    output += `
-      <div class="card filterDiv ${typeClass} show">
-        <h1>${p.name}</h1>
-        <h3>Type: ${p.type}</h3>
-        <h4>Weight: ${p.weight} kg</h4>
-        <h4>Height: ${p.height} m</h4>
-        <h4>Evolution: ${p.evolutionStage}</h4>
-        <hr>
-      </div>`;
+  for (let i = 0; i < pokemon.length; i++) {
+    let poke = pokemon[i];
+
+    build += '<div class="card">';
+    build += `<h1>Name: ${poke.Name}</h1>`;
+    build += `<h3>Main type: ${poke.Type}</h3>`;
+    build += `<h4>Weight: ${poke.Weight} lbs</h4>`;
+    build += `<h4>Height: ${poke.Height} meters</h4>`;
+    build += `<img src="images/${poke.Name}.png">`;
+    build += `<hr>`;
+    build += '</div>';
   }
 
-  document.getElementById("centerpanel").innerHTML = output;
+  output.innerHTML = build;
 }
 
-function filterSelection(button, category) {
-  let cards = document.getElementsByClassName("filterDiv");
+function filter_pokemon() {
+  let name = document.getElementById("filter").value;
+  console.log(name);
 
-  for (let card of cards) {
-    card.classList.remove("show");
-    if (category === "all" || card.classList.contains(category)) {
-      card.classList.add("show");
+  let new_pokemon = [];
+
+  for (let i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].Name == name) {
+      new_pokemon.push(pokemon[i]);
     }
   }
 
-  for (let btn of document.getElementsByClassName("btn")) {
-    btn.classList.remove("active");
-  }
-  button.classList.add("active");
+  generateCards(new_pokemon);
 }
